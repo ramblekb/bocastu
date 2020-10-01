472352fae1e81230803df26055aefcafcdedd4ae
ramblekb <ramblekb@gmail.com>
2020-10-01 04:56:22 -0500

    new hompage with gram

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+
+
+// var granimInstance = new Granim({
+//     element: '#canvas-basic',
+//     direction: 'radial',
+//     isPausedWhenNotInView: true,
+//     states : {
+//         "default-state": {
+//             gradients: [
+//                 ['#ff9966', '#ff5e62'],
+//                 ['#00F260', '#0575E6'],
+//                 ['#e1eec3', '#f05053']
+//             ]
+//         }
+//     }
+// });
+
 var granimInstance = new Granim({
     element: '#canvas-basic',
-    direction: 'radial',
+    direction: 'left-right',
     isPausedWhenNotInView: true,
     states : {
         "default-state": {
             gradients: [
-                ['#ff9966', '#ff5e62'],
-                ['#00F260', '#0575E6'],
-                ['#e1eec3', '#f05053']
+                [
+                    { color: '#833ab4', pos: .2 },
+                    { color: '#fd1d1d', pos: .8 },
+                    { color: '#38ef7d', pos: 1 }
+                ], [
+                    { color: '#40e0d0', pos: 0 },
+                    { color: '#ff8c00', pos: .2 },
+                    { color: '#ff0080', pos: .75 }
+                ],
             ]
         }
     }
